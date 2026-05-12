da426077438817bb92d749967eb77f93cc85eaec
bbb1293 <ball1041503@gmail.com>
2026-05-13 03:59:40 +0900

    neo-tree: pin to left, auto-open on startup, auto-reveal current file

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -16,7 +16,14 @@ vim.pack.add(plugins)
 vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
 require('neo-tree').setup {
+  close_if_last_window = true, -- close nvim if neo-tree is the last window left
+  window = {
+    position = 'left',
+    width = 30,
+  },
   filesystem = {
+    follow_current_file = { enabled = true }, -- auto-reveal current file as you switch buffers
+    use_libuv_file_watcher = true,            -- live-update tree when files change on disk
     window = {
       mappings = {
         ['\\'] = 'close_window',
@@ -24,3 +31,12 @@ require('neo-tree').setup {
     },
   },
 }
+
+-- Auto-open neo-tree on startup, pinned to the left.
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    -- Skip if nvim was opened with a file argument and you just want to edit
+    -- (still opens, but doesn't grab focus)
+    vim.cmd 'Neotree show'
+  end,
+})
